2d7237d1dc0f3c08c5eb4acad403b88346ab5dc6
Kaleb <Kalebalebachew4@gmail.com>
2023-12-25 00:25:05 +0300

    Rest-API's for goals

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const app = express()
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middlewares/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 
 app.use(express.json())
